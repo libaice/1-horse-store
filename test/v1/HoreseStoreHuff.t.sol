41b121818989dfaf47964975311bedeb8b75f2c2
libaice <libaice147@gmail.com>
2024-04-14 23:00:09 +0800

    init Base Test abstact

---
+++ test/v1/HoreseStoreHuff.t.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.20;
+
+import {Base_TestV1} from "./Base_TestV1.t.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+
+contract HorseStoreHuff is Base_TestV1 {
+    string public constant HORSE_STORE_HUFF_LOCATION = "horseStoreV1/horsestore";
+
+    // function setUp() public override {}
+}
